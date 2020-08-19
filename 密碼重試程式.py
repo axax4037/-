8f2a22d3e01439fc66231e093d38d13e71cb28ec
axax4037 <mschen1116@gmail.com>
2020-08-19 19:13:51 +0800

    x > 0

--- 密碼重試程式.py
+++ 密碼重試程式.py
@@ -6,13 +6,16 @@
 # 如果布正確 print "密碼錯誤！ 還有__此機會！"
 password = "a123456"
 x = 3
-while True:
+while x > 0:
+    x = x - 1
     pwd = input("請輸入密碼: ")
     if pwd == password:
         print("登入成功")
         break
     else:
-        x = x - 1
-        print("密碼錯誤！ 還有", x,"此機會！")
-        if x == 0:
-           break
+        print("密碼錯誤！")
+        if x > 0:
+            print("還有", x,"次機會")
+        else:
+            print("沒有機會了，再見")
+            break
